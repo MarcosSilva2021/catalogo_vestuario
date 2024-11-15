5e9cbffcf73239bc54b29d63bdb531c1d12ee512
Marcosdsj <mfsilva5585@gmail.com>
2024-11-15 19:59:15 -0300

    first commit

---
+++ src/main/java/com/dev/catalogo_vestuario/CatalogoVestuarioApplication.java
@@ -0,0 +1,13 @@
+package com.dev.catalogo_vestuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoVestuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoVestuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/catalogo_vestuario/CatalogoVestuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.catalogo_vestuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoVestuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
