cae6a4c6c07bcc3a7c9e2caf28218fe4ca621d9b
Marcosdsj <mfsilva5585@gmail.com>
2024-11-16 21:16:51 -0300

    add vestuario-service

---
+++ src/main/java/com/dev/catalogo_vestuario/service/VestuarioService.java
@@ -0,0 +1,19 @@
+package com.dev.catalogo_vestuario.service;
+
+import java.util.List;
+
+import com.dev.catalogo_vestuario.models.Vestuario;
+
+public interface VestuarioService {
+
+    Vestuario findById(String uuid);
+
+    Vestuario addVestuario(Vestuario vestuario);    
+
+    List<Vestuario> findAll();
+    
+    Vestuario update(String id, Vestuario vestuarioToUpdate);
+
+    void delete( String uuid) ;
+
+}
