510394591cdf0d998203919145a3ccc699713799
Marcosdsj <mfsilva5585@gmail.com>
2024-11-16 22:39:44 -0300

    add Anotacion Rest

--- src/main/java/com/dev/catalogo_vestuario/controllers/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dev/catalogo_vestuario/controllers/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.dev.catalogo_vestuario.controllers.exception;
 
-import com.dev.catalogo_vestuario.models.Vestuario;
-
 import java.util.NoSuchElementException;
 
 import org.slf4j.Logger;
@@ -9,8 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
