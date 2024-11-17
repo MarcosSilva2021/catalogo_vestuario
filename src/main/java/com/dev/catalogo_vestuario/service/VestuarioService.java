d0a73a594b2abf658982f5def2392fb3baf0a606
Marcosdsj <mfsilva5585@gmail.com>
2024-11-16 21:24:00 -0300

    add vestuario service up

--- src/main/java/com/dev/catalogo_vestuario/service/VestuarioService.java
+++ src/main/java/com/dev/catalogo_vestuario/service/VestuarioService.java
@@ -16,4 +16,9 @@ public interface VestuarioService {
 
     void delete( String uuid) ;
 
+    // Associa uma marca a um vestuario
+    Vestuario adddMarca(String idVestuario, String idMarca);
+
+    // Associa uma Categoria a um vestuario
+    Vestuario addCategoria(String idVestuario, String idCategoria);
 }
