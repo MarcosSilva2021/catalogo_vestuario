16665891bb9dc5f4e01f5232dc0563bf57e9e62b
Marcosdsj <mfsilva5585@gmail.com>
2024-11-17 01:13:22 -0300

    add swagger

--- src/main/java/com/dev/catalogo_vestuario/CatalogoVestuarioApplication.java
+++ src/main/java/com/dev/catalogo_vestuario/CatalogoVestuarioApplication.java
@@ -3,6 +3,10 @@ package com.dev.catalogo_vestuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class CatalogoVestuarioApplication {
 
